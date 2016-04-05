429ea4a167505f00d86d331df0087abaf149c32f
lizhen325 <zhenli336@gmail.com>
2016-04-05 22:31:10 +1200

    Heap Sort

---
+++ MIT/Heap/Heap/HeapSorting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class HeapSorting
+    {
+        public void MaxHeapify(int[] nums, int currentIndex, int heapSize)
+        {
+            int leftChild = 2 * currentIndex;
+            int rightChild = 2 * currentIndex + 1;
+            int max = currentIndex;
+            if(leftChild < heapSize && nums[leftChild] > nums[max])
+            {
+                max = leftChild;
+            }
+            if(rightChild < heapSize && nums[rightChild] > nums[max])
+            {
+                max = rightChild;
+            }
+            if(currentIndex != max)
+            {
+                int temp = nums[currentIndex];
+                nums[currentIndex] = nums[max];
+                nums[max] = temp;
+                MaxHeapify(nums, max, heapSize);
+            }
+        }
+
+        public void BuildHeap(int[] nums)
+        {
+            for (int i = nums.Length/2 -1; i >= 0; i--)
+                MaxHeapify(nums, i,nums.Length);
+        }
+
+        public void HeapSort(int[] nums)
+        {
+            BuildHeap(nums);
+            for (int i = nums.Length - 1; i >0; i--)
+            {
+                int temp = nums[0];
+                nums[0] = nums[i];
+                nums[i] = temp;
+                MaxHeapify(nums, 0 , i-1);
+            }
+        }
+    }
+}
---
+++ MIT/Heap/Heap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1,3,5,3,6,3,4,1,7 };
+            HeapSorting heapsort = new HeapSorting();
+            heapsort.HeapSort(nums);
+            for (int i = 0; i < nums.Length; i++)
+                Console.Write(nums[i]);
+            Console.ReadKey();
+        }
+    }
+}
