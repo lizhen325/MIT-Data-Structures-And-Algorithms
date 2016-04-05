32cb08ea203da92fb83f192035957a2b7ef7c61f
lizhen325 <zhenli336@gmail.com>
2016-04-05 16:38:55 +1200

    Insertion Sort

---
+++ InsertionSortAndMergeSort/InsertionSortAndMergeSort/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortAndMergeSort
+{
+    public class InsertionSort
+    {
+        public static int[] InsertionSorting(int[] nums)
+        {
+            for(int i=1; i<nums.Length; i++)
+            {
+                for(int j=i; j>0; j--)
+                {
+                    if(nums[j] < nums[j-1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j - 1];
+                        nums[j - 1] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ InsertionSortAndMergeSort/InsertionSortAndMergeSortTests/InsertionSortTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InsertionSortAndMergeSort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortAndMergeSort.Tests
+{
+    [TestClass()]
+    public class InsertionSortTests
+    {
+        [TestMethod()]
+        public void InsertionSortingTest()
+        {
+            int[] nums = { 3, 4, 5, 3, 2, 1 };
+            int[] res = InsertionSort.InsertionSorting(nums);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 3, 4, 5 }, res);
+        }
+    }
+}
