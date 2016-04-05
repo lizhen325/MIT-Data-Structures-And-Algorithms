fae102c6b650e8f691cd14e7d02627888b473434
lizhen325 <zhenli336@gmail.com>
2016-04-05 18:04:44 +1200

    Merge Sort

---
+++ MIT/InsertionSortAndMergeSort/InsertionSortAndMergeSort/MergeSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortAndMergeSort
+{
+    public class MergeSort
+    {
+        public void MergeSorting(int[] unsorted, int first, int mid, int last, int[]sorted)
+        {
+            int i = first, j = mid;
+            int k = 0;
+        
+            while (i < mid && j < last)
+                if (unsorted[i] < unsorted[j])
+                    sorted[k++] = unsorted[i++];
+                else
+                    sorted[k++] = unsorted[j++];
+
+            //Merge the elements in left array
+            while (i < mid)
+                sorted[k++] = unsorted[i++];
+            //merge the elements in right array
+            while (j < last)
+                sorted[k++] = unsorted[j++];
+            //move from sorted array
+            for (int v = 0; v < k; v++)
+                unsorted[first + v] = sorted[v];
+        }
+
+        public void Merge(int[] unsorted, int first, int last, int []sorted)
+        {
+            if (first + 1 < last)
+            {
+                int mid = (first + last) / 2;
+                //merge elements left array
+                Merge(unsorted, first, mid, sorted);
+                //merge elements right array
+                Merge(unsorted, mid, last, sorted);
+                //merge
+                MergeSorting(unsorted, first, mid, last, sorted);
+            }
+        }
+    }
+}
--- MIT/InsertionSortAndMergeSort/InsertionSortAndMergeSort/Program.cs
+++ MIT/InsertionSortAndMergeSort/InsertionSortAndMergeSort/Program.cs
@@ -10,6 +10,13 @@ namespace InsertionSortAndMergeSort
     {
         static void Main(string[] args)
         {
+            int[] nums = { 1, 3, 5, 7, 3, 2, 4, 6, 8 };
+            int[] sorted = new int[nums.Length];
+            MergeSort mearge = new MergeSort();
+            mearge.Merge(nums,0,nums.Length,sorted);
+            for (int i = 0; i < nums.Length; i++)
+                Console.WriteLine(nums[i]);
+            Console.ReadKey();
         }
     }
 }
