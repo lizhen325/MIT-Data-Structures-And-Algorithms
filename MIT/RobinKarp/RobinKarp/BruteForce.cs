ddcef0aaad973d83ce581424f7ce83410a0fb63d
lizhen325 <zhenli336@gmail.com>
2016-04-07 23:38:33 +1200

    Brute Force Algorithm

---
+++ MIT/RobinKarp/RobinKarp/BruteForce.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobinKarp
+{
+    public class BruteForce
+    {
+        public static void FindWord(string words, string pattern)
+        {
+            int n = words.Length;
+            int m = pattern.Length;
+            if (n >= m)
+            {
+                for (int i = 0; i <= n - m; i++)
+                {
+                    int j = 0;
+                    while (j < m && pattern[j] == words[i+j])
+                    {
+                        j++;
+                    }
+                    if(j==m)
+                    {
+                        Console.WriteLine("found");
+                        return;
+                    }
+                }
+                Console.WriteLine("not found");
+            }
+        }
+    }
+}
---
+++ MIT/RobinKarp/RobinKarp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobinKarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "she shell me";
+            string pattern = "shele";
+            BruteForce.FindWord(words, pattern);
+            Console.ReadKey();
+        }
+    }
+}
