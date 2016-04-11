57b124023cfe9f543b8f4a24bba5e29083f5dba7
lizhen325 <zhenli336@gmail.com>
2016-04-12 01:50:54 +1200

    Fibonacci numbers

---
+++ MIT/DynamicProgramming/DynamicProgramming/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    public class Fibonacci
+    {
+        public long[] FibonacciNumbers(int n)
+        {
+            long[] fibo = new long[n + 1];
+            fibo[0] = 0;
+            fibo[1] = 1;
+            for (int i = 2; i < n + 1; i++)
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+            return fibo;
+        }
+    }
+}
---
+++ MIT/DynamicProgramming/DynamicProgramming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            long [] res = fibo.FibonacciNumbers(50);
+            foreach (var i in res)
+                Console.Write(i + ", ");
+            Console.ReadKey();
+        }
+    }
+}
